3192ccba127479c0c6a9f2b11f5deec04a322b37
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-10-25 11:44:32 -0500

    Create Yourmom.java

---
+++ Yourmom.java
@@ -0,0 +1,20 @@
+class YourMom {
+enum YourMomsWeight {Fat,Fluffy,Anorexic,Normal,OH MEH GERSH IT HAS ITS OWN GRAVITY}
+YouMomsWeight weight;
+}
+public class Scale {
+public static void main(String args[])
+{
+YourMom mom = new YourMom();
+if (weight == Fat);
+then (System.out.println("Yo momma need to lose some weight);
+if (weight == Fluffy);
+then (System.out.println("Yo momma aint fat, she be fluffy!");
+if (weight == Anorexic);
+then System.out.println("Yo momma needs some cheeze burgers");
+if (weight == Normal);
+then (System.out.println("Yo mommas just fine meh dude");
+if (weight == OH MEH GERSH IT HAS ITS OWN GRAVITY);
+then System.out.println("GET IT ON A TREADMILL");
+   }
+}

9ee73cfd3cb198202e03c3ef5ef0dda88b66fd5c
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-10-30 10:32:04 -0500

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,6 +1,13 @@
 public class Math {
+static double a = 30;
+  static double b = -12;
+  static double c = 3;
+  static double d = 2;
+  static double first = a + b / c * d;
+  
+   
 
 public static void main (String args[]) {
-
+ System.out.println(first);
 }
 }

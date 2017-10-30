04583cb0b16062814346909bd6f083429c61011c
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-10-30 13:14:40 -0500

    Create Math3.java

---
+++ Math3.java
@@ -0,0 +1,15 @@
+public class Math3 {
+static double a = -2;
+static double b = 4;
+static double c = -6;
+static double d = -4;
+static var x = "x";
+static double step1 = a * x;
+static double step2 = c + d;
+static double step3 = step2 / a;
+static double step4 = 
+public static void main (String args[]) {
+System.out.println(x);
+
+}
+}

85fa28be7fd5c1d59c9b49f87f9d2c2e53f315fa
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-10-30 11:18:11 -0500

    Update Math2.java

--- Math2.java
+++ Math2.java
@@ -9,11 +9,9 @@ static double aa = c + d;
 static double bb = f + g;
 static double aaa = b * aa;
 static double bbb = a + aaa;
-static double ccc = java.lang.Math.abs(double bbb);
-static double fg = java.lang.Math.abs(double f + g);
-static double answer = ccc - fg;
+
 
 public static void main (String args[]) {
-System.out.println(answer);
+System.out.println(abs(aaa) -  abs(bb) );
 }
 }

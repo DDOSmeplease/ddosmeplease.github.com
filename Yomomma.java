ffe27daa4de532f54aeb4d4eee4e45f28e48f84e
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-11-01 11:13:51 -0500

    Update Yomomma.java

--- Yomomma.java
+++ Yomomma.java
@@ -1,26 +1,33 @@
 public class Dog{
 
-int age;
-double weight;
+int Age;
+double Weight;
 
-public Dog(String breed) {
-System.out.println("Your dogs breed is" + breed);
+public Dog(String Breed) {
+System.out.println("Your dogs breed is" + Breed);
 }
-public Dog(String color) {
-System.out.print.ln("Your dog is" + color);
+public Dog(String Color) {
+System.out.print.ln("Your dog is" + Color);
 }
-public Dog(int age) {
-System.out.println("Your dog is" + age + "years old");
+public Dog(int Age) {
+System.out.println("Your dog is" + Age + "years old");
 }
-public Dog(double weight) {
-System.out.println("Your dog weighs" + weight + "pounds")
+public Dog(double Weight) {
+System.out.println("Your dog weighs" + Weight + "pounds")
 }
 public static void main (String args[]) {
 Dog myDog = new Dog ("Sparky");
-myDog.setbreed("Golden Retreiver);
-myDog.setcolor("Golden Brown");
+myDog.setBreed("Golden Retreiver);
+               myDog.getBreed();
+myDog.setColor("Golden Brown");
+               myDog.getColor();
 myDog.setage(4);
-myDog.setweight(50.12);
-
+               myDog.getage();
+myDog.setWeight(50.12);
+myDog.getWeight();
+               System.out.println(myDog.dogBreed);
+               System.out.println(myDog.dogColor);
+               System.out.println(myDog.dogAge);
+               System.out.println(myDog.dogWeight);
 }
 }

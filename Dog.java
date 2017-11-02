669c64d2d7c055a1188733768d92ee9d18084511
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-11-02 17:14:25 -0500

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,31 +1,44 @@
-public class Dog{
-
-
-public Dog(String Breed) {
-System.out.println("Your dogs breed is" + Breed);
-}
-public Dog(String Color) {
-System.out.println("Your dog is" + Color);
-}
-public Dog(int Age) {
-System.out.println("Your dog is" + Age + "years old");
-}
-public Dog(double Weight) {
-System.out.println("Your dog weighs" + Weight + "pounds");
-}
-public static void main (String args[]) {
-Dog myDog = new Dog ("Sparky");
-myDog.setBreed("Golden Retreiver");
-               myDog.getBreed();
-myDog.setColor("Golden Brown");
-               myDog.getColor();
-myDog.setage(4);
-               myDog.getage();
-myDog.setWeight(50.12);
-myDog.getWeight();
-               System.out.println(myDog.dogBreed);
-               System.out.println(myDog.dogColor);
-               System.out.println(myDog.dogAge);
-               System.out.println(myDog.dogWeight);
-}
+public class Dog {
+  
+  String name, color, breed;
+  int age;
+  double weight;
+  
+  public Dog(String name) {
+    System.out.println("Your dog\'s name is " + name);
+    this.name = name;
+  }
+  
+  public void setBreed(String breed){
+    System.out.println("your dog's breed is " + breed);
+    this.breed = breed;
+  }
+  
+  public void setColor(String color) {
+    System.out.println("Your dog is " + color);
+    this.color = color;
+  }
+  
+  public void setAge(int age) {
+    System.out.println("Your dog is " + age + " years old");
+    this.age = age;
+  }
+  
+  public void setWeight(double weight) {
+    System.out.println("Your dog weighs " + weight + " pounds");
+    this.weight = weight;
+  }
+  
+  public static void main (String args[]) {
+    Dog myDog = new Dog ("Sparky");
+    myDog.setBreed("Golden Retreiver");
+    myDog.setColor("Golden Brown");
+    myDog.setAge(4);
+    myDog.setWeight(50.12);
+    System.out.println(myDog.name);
+    System.out.println(myDog.breed);
+    System.out.println(myDog.color);
+    System.out.println(myDog.age);
+    System.out.println(myDog.weight);
+  }
 }

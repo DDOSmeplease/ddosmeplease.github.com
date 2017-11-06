fd937a46e3d83bc4c3865bbc31edfa6094840822
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2017-11-06 14:05:20 -0600

    Create Duck.java

---
+++ Duck.java
@@ -0,0 +1,38 @@
+public class Duck {
+String color, name;
+int age;
+double weight, height;
+
+public Duck(String name) {
+System.out.println("Your duck's name is " + name);
+this.name = name;
+}
+public void setAge(int age) {
+System.out.println("Your duck's age is " + age);
+this.age = age;
+}
+public void setWeight(double weight) {
+System.out.println("Your duck weighs " + weight);
+this.weight = weight;
+}
+public void setHeight(double height) {
+System.out.println("Your duck is " + height + " feet tall");
+this.height = height;
+}
+public void setColor(String color) {
+System.out.println("Your duck is " + color);
+this.color = color;
+}
+public static void main(String args[]) {
+Duck myDuck = new Duck("James");
+myDuck.setAge(4);
+myDuck.setWeight(30.55);
+myDuck.setHeight(2.11);
+myDuck.setColor("White");
+  System.out.println(myDuck.age);
+  System.out.println(myDuck.color);
+  System.out.println(myDuck.weight);
+  System.out.println(myDuck.height);
+  }
+}
+

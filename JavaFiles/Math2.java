beba9adaf371aa85c41ef8c0da59d99a5dac0a43
DDOSmeplease <32173992+DDOSmeplease@users.noreply.github.com>
2018-01-26 12:00:26 -0600

    Update Math2.java

--- JavaFiles/Math2.java
+++ JavaFiles/Math2.java
@@ -1,22 +1,11 @@
-public class Math2 {
-static double a = 4;
-static double b = -8;
-static double c = 3;
-static double d = -12;
-static double f = 5;
-static double g = -11;
-static double aa = c + d;
-static double bb = f + g;
-static double aaa = b * aa;
-static double bbb = a + aaa;
-static double f1 = Math.abs(aaa);
-  static double f2 = Math.abs(bb);
-
-public static void main (String args[]) {
-
-  
-  
-  
-  System.out.println(f1 - f2 );
-}
-}
+import java.util.Scanner;  
+class ScannerTest{  
+ public static void main(String args[]){  
+   Scanner sc=new Scanner(System.in);  
+     
+   System.out.println("Enter your name");  
+   var name=sc.nextInt();   
+   System.out.println("Hey " + name + " welcome!");  
+   sc.close();  
+ }  
+}   
